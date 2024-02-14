1a17cc652f282810b82edc46b3311649c1d77b47
kevinmelodi <128565421+kevinmelodi@users.noreply.github.com>
2024-02-14 08:25:43 +0000

    fake streaming

--- pages/6_assistants.py
+++ pages/6_assistants.py
@@ -2,7 +2,13 @@ from openai import OpenAI
 import streamlit as st
 import time
 
-st.title("ChatGPT-like clone")
+st.title("OpenAI Assistants API toy demo")
+
+# Streamed response emulator
+def fake_stream(message):
+    for word in message.split():
+        yield word + " "
+        time.sleep(0.05)
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 assistantId = 'asst_dzvy5mIYrwNScAeFUqnN9Llt'
@@ -13,7 +19,7 @@ if "thread" not in st.session_state:
     threadId = str(thread.id)
     st.session_state['thread'] = threadId
     st.session_state['thread']
-st.write(st.session_state)
+
 
 
 thread_messages = client.beta.threads.messages.list(st.session_state['thread'])
@@ -45,7 +51,7 @@ if prompt := st.chat_input("What is up?"):
 
     with st.spinner('Please wait while the assistant processes your input...'):
         while run.status != "completed":
-            time.sleep(5)  # Adjust sleep time if needed
+            time.sleep(.1)  # Adjust sleep time if needed
             run = client.beta.threads.runs.retrieve(
                 thread_id=st.session_state['thread'], run_id=run.id
             )
@@ -60,4 +66,5 @@ if prompt := st.chat_input("What is up?"):
     for message in reversed(new_thread_messages.data):
         if message.id in new_message_ids:
             with st.chat_message(message.role):
-                st.markdown(message.content[0].text.value)
+                st.write_stream(fake_stream(message.content[0].text.value))
+
